1c4f8a30ebfe9c3c5ec3c16eb1b2dc7cfe0519ae
hulbrt-b <benhulbrt@gmail.com>
2025-06-19 21:13:57 -0700

    Update scheduler.py
    Inital

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,11 @@
+from celery.schedules import crontab
+from tasks import celery, clone_disk
+
+celery.conf.beat_schedule = {
+    'weekly-clone': {
+        'task': 'tasks.clone_disk',
+        'schedule': crontab(hour=3, minute=0, day_of_week='sun'),
+        'args': ['source.example.com', '/mnt/weekly_backup/disk.img']
+    }
+}
 
