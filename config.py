37e0fc80dbf6626b7e32827c94a3dcfeccc9c487
hulbrt-b <benhulbrt@gmail.com>
2025-06-19 21:31:52 -0700

    Update config.py
    Updated destinations

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
-REMOTE_HOSTS = {
-    'server1': {'ip': '10.1.1.1', 'device': '/dev/sda'},
-    'server2': {'ip': '10.1.1.2', 'device': '/dev/nvme0n1'}
+# config.py
+
+SERVERS = {
+    "remote1": {
+        "host": "192.168.1.101",
+        "disk": "/dev/sdb"
+    },
+    "remote2": {
+        "host": "192.168.1.102",
+        "disk": "/dev/sdc"
+    },
+    "remote3": {
+        "host": "192.168.1.103",
+        "disk": "/dev/sdd"
+    }
 }
 
+# Default user to SSH as (optional if you want to make this dynamic later)
+SSH_USER = "root"
